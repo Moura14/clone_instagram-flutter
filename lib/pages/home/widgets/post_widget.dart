fd272277514c18c805fe0be232a87919741a526c
João Gabriel <jgmoura.dev@gmail.com>
2023-02-23 19:39:21 -0300

    finalização do projeto

--- lib/pages/home/widgets/post_widget.dart
+++ lib/pages/home/widgets/post_widget.dart
@@ -8,7 +8,7 @@ class PostWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final desktop = ResponsiveWrapper.of(context).isDesktop;
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: desktop ? 35 : 0),
+      padding: EdgeInsets.symmetric(vertical: desktop ? 16 : 0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
